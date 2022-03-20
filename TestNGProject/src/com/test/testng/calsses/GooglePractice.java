a948a0df612b42cbe1a11cd44c07c7131b76ac34
Shree <Shree@DESKTOP-FAAJO8O>
2022-03-20 15:40:42 +0530

    gdfgg

--- TestNGProject/src/com/test/testng/calsses/GooglePractice.java
+++ TestNGProject/src/com/test/testng/calsses/GooglePractice.java
@@ -1,5 +1,17 @@
 package com.test.testng.calsses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class GooglePractice {
+	
+	
+    @Test
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "E:\\\\WORKSPACE\\\\SeleniumProject\\\\Resources\\\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	}
 
 }
